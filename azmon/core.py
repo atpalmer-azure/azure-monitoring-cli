b56974708825f5989ec60f7deb290906411cb3c9
Andy Palmer <andrew.t.palmer@parker.com>
2019-03-24 14:33:04 -0500

    Derive ru_per_s from total_request_units

--- azmon/core.py
+++ azmon/core.py
@@ -59,13 +59,24 @@ class CosmosMetrics(object):
     def __init__(self, metrics):
         self._metrics = metrics
 
+    def total_request_units(self):
+        """
+        From: https://docs.microsoft.com/en-us/azure/azure-monitor/platform/metrics-supported
+        Used to monitor Total RU usage at a minute granularity. To get average RU consumed per second, use Total aggregation at minute and divide by 60.
+        """
+        return self._metrics.list_metrics('TotalRequestUnits', 'Total')
+
     def ru_per_s(self):
-        data = self._metrics.list_metrics('TotalRequestUnits', 'Total')
-
-        # From: https://docs.microsoft.com/en-us/azure/azure-monitor/platform/metrics-supported
-        # Used to monitor Total RU usage at a minute granularity. To get average RU consumed per second, use Total aggregation at minute and divide by 60.
-
+        """
+        Custom metric derived from total_request_units
+        """
+        data = self.total_request_units()
         return [ { **item, 'RUs': item['total'] / 60 } for item in data ]
 
     def data_usage(self):
+        """
+        From: https://docs.microsoft.com/en-us/azure/azure-monitor/platform/metrics-supported
+        Used to monitor total data usage at collection and region, minimum granularity should be 5 minutes.
+
+        """
         return self._metrics.list_metrics('DataUsage', 'Total')
