e9d840edea9f75dd0af6b0b2f8b3a6d152fe02b3
Andy Palmer <andrew.t.palmer@parker.com>
2019-03-24 15:16:33 -0500

    Rename command group parameter

--- azmon/cli/cosmos.py
+++ azmon/cli/cosmos.py
@@ -2,29 +2,29 @@ import click
 from ..tools import clicktools
 
 
-def commands(cosmos):
-    @cosmos.command()
+def commands(group):
+    @group.command()
     @click.pass_obj
     @clicktools.handle_result
     def rups(metrics_client):
         """Cosmos DB Request Units per Second"""
         return metrics_client.ru_per_s()
 
-    @cosmos.command()
+    @group.command()
     @click.pass_obj
     @clicktools.handle_result
     def ru(metrics_client):
         """Cosmos DB Request Units per Minute"""
         return metrics_client.total_request_units()
 
-    @cosmos.command()
+    @group.command()
     @click.pass_obj
     @clicktools.handle_result
     def data(metrics_client):
         """Cosmos DB total data usage"""
         return metrics_client.data_usage()
 
-    @cosmos.command()
+    @group.command()
     @click.pass_obj
     @clicktools.handle_result
     def documents(metrics_client):
