bc4d5fa11d0183c4394b6443795dd656f9522f44
Andy Palmer <andrew.t.palmer@parker.com>
2019-03-24 16:50:56 -0500

    Add count CLI option

--- azmon/cli/cosmos.py
+++ azmon/cli/cosmos.py
@@ -4,57 +4,65 @@ from ..tools import clicktools
 
 def commands(group):
     @group.command()
+    @click.option('-c', '--count', default=1)
     @click.pass_obj
     @clicktools.handle_result
-    def requests(metrics_client):
+    def requests(metrics_client, **kwargs):
         """Cosmos DB metadata requests"""
-        return metrics_client.total_requests()
+        return metrics_client.total_requests(**kwargs)
 
     @group.command()
+    @click.option('-c', '--count', default=1)
     @click.pass_obj
     @clicktools.handle_result
-    def metareq(metrics_client):
+    def metareq(metrics_client, **kwargs):
         """Cosmos DB metadata requests"""
-        return metrics_client.metadata_requests()
+        return metrics_client.metadata_requests(**kwargs)
 
     @group.command()
+    @click.option('-c', '--count', default=1)
     @click.pass_obj
     @clicktools.handle_result
-    def rups(metrics_client):
+    def rups(metrics_client, **kwargs):
         """Cosmos DB Request Units per Second"""
-        return metrics_client.ru_per_s()
+        return metrics_client.ru_per_s(**kwargs)
 
     @group.command()
+    @click.option('-c', '--count', default=1)
     @click.pass_obj
     @clicktools.handle_result
-    def ru(metrics_client):
+    def ru(metrics_client, **kwargs):
         """Cosmos DB Request Units per Minute"""
-        return metrics_client.total_request_units()
+        return metrics_client.total_request_units(**kwargs)
 
     @group.command()
+    @click.option('-c', '--count', default=1)
     @click.pass_obj
     @clicktools.handle_result
-    def provisioned(metrics_client):
+    def provisioned(metrics_client, **kwargs):
         """Cosmos DB provisioned RU/s throughput"""
-        return metrics_client.provisioned_throughput()
+        return metrics_client.provisioned_throughput(**kwargs)
 
     @group.command()
+    @click.option('-c', '--count', default=1)
     @click.pass_obj
     @clicktools.handle_result
-    def data(metrics_client):
+    def data(metrics_client, **kwargs):
         """Cosmos DB total data usage"""
-        return metrics_client.data_usage()
+        return metrics_client.data_usage(**kwargs)
 
     @group.command()
+    @click.option('-c', '--count', default=1)
     @click.pass_obj
     @clicktools.handle_result
-    def index(metrics_client):
+    def index(metrics_client, **kwargs):
         """Cosmos DB total data usage"""
-        return metrics_client.index_usage()
+        return metrics_client.index_usage(**kwargs)
 
     @group.command()
+    @click.option('-c', '--count', default=1)
     @click.pass_obj
     @clicktools.handle_result
-    def documents(metrics_client):
+    def documents(metrics_client, **kwargs):
         """Cosmos DB document count"""
-        return metrics_client.document_count()
+        return metrics_client.document_count(**kwargs)
