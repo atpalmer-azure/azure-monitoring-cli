1d7657470b1c95d7fa6f2a874ed9b9222055a766
Andy Palmer <andrew.t.palmer@parker.com>
2019-03-26 16:32:50 -0500

    Add logio command

--- azmon/cli/sql.py
+++ azmon/cli/sql.py
@@ -6,3 +6,8 @@ def commands(group):
     def cpu(metrics_client, **kwargs):
         """CPU percentage"""
         return metrics_client.cpu_percent(**kwargs)
+
+    @clicktools.metric_command(group)
+    def logio(metrics_client, **kwargs):
+        """Log IO percentage"""
+        return metrics_client.log_write_percent(**kwargs)
--- azmon/metrics/sql.py
+++ azmon/metrics/sql.py
@@ -9,3 +9,9 @@ class SqlMetrics(object):
         From: https://docs.microsoft.com/en-us/azure/azure-monitor/platform/metrics-supported
         """
         return self._metrics.list_metrics('cpu_percent', 'Total', **kwargs)
+
+    def log_write_percent(self, **kwargs):
+        """
+        From: https://docs.microsoft.com/en-us/azure/azure-monitor/platform/metrics-supported
+        """
+        return self._metrics.list_metrics('log_write_percent', 'Total', **kwargs)
