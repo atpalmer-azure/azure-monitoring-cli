4640a61e2abdab706f2e317a321caafc26431cf3
Andy Palmer <andrew.t.palmer@parker.com>
2019-03-26 14:53:18 -0500

    Update IoT Hub documentation strings

--- azmon/cli/iothub.py
+++ azmon/cli/iothub.py
@@ -4,11 +4,11 @@ from ..tools import clicktools
 def commands(group):
     @clicktools.metric_command(group)
     def inall(metrics_client, **kwargs):
-        """IoT Hub telemetry message send attempts"""
+        """Number of device-to-cloud telemetry messages attempted to be sent to your IoT hub"""
         return metrics_client.ingress_all(**kwargs)
 
     @clicktools.metric_command(group)
     @clicktools.rename('in')
     def in_(metrics_client, **kwargs):
-        """IoT Hub telemetry messages sent"""
+        """Number of device-to-cloud telemetry messages sent successfully to your IoT hub"""
         return metrics_client.ingress_success(**kwargs)
