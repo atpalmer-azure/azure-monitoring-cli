c9307115f94ceaa7ee6b89c3c652ee26660089dc
Andy Palmer <andrew.t.palmer@parker.com>
2019-03-26 14:58:59 -0500

    Add IoT Hub egress commands

--- azmon/cli/iothub.py
+++ azmon/cli/iothub.py
@@ -12,3 +12,18 @@ def commands(group):
     def in_(metrics_client, **kwargs):
         """Number of device-to-cloud telemetry messages sent successfully to your IoT hub"""
         return metrics_client.ingress_success(**kwargs)
+
+    @clicktools.metric_command(group)
+    def commands(metrics_client, **kwargs):
+        """Number of cloud-to-device commands completed successfully by the device"""
+        return metrics_client.egress_success(**kwargs)
+
+    @clicktools.metric_command(group)
+    def abandoned(metrics_client, **kwargs):
+        """Number of cloud-to-device commands abandoned by the device"""
+        return metrics_client.egress_abandoned(**kwargs)
+
+    @clicktools.metric_command(group)
+    def rejected(metrics_client, **kwargs):
+        """Number of cloud-to-device commands rejected by the device"""
+        return metrics_client.egress_rejected(**kwargs)
--- azmon/metrics/iothub.py
+++ azmon/metrics/iothub.py
@@ -15,3 +15,21 @@ class IotHubMetrics(object):
         From: https://docs.microsoft.com/en-us/azure/azure-monitor/platform/metrics-supported
         """
         return self._metrics.list_metrics('d2c.telemetry.ingress.success', 'Total', **kwargs)
+
+    def egress_success(self, **kwargs):
+        """
+        From: https://docs.microsoft.com/en-us/azure/azure-monitor/platform/metrics-supported
+        """
+        return self._metrics.list_metrics('c2d.commands.egress.complete.success', 'Total', **kwargs)
+
+    def egress_abandoned(self, **kwargs):
+        """
+        From: https://docs.microsoft.com/en-us/azure/azure-monitor/platform/metrics-supported
+        """
+        return self._metrics.list_metrics('c2d.commands.egress.abandon.success', 'Total', **kwargs)
+
+    def egress_rejected(self, **kwargs):
+        """
+        From: https://docs.microsoft.com/en-us/azure/azure-monitor/platform/metrics-supported
+        """
+        return self._metrics.list_metrics('c2d.commands.egress.reject.success', 'Total', **kwargs)
